197aaf4ec6058ed634cffa2f23e250a6c02b3d5f
ibra <albakov.ibraguim@gmail.com>
2024-12-23 14:11:45 +0100

    ❇️ Created app localization feature

---
+++ lib/src/core/locale/l10n.dart
@@ -0,0 +1,21 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final l10nProvider = Provider<AppLocalizations>((ref) {
+  final locale = L10n.defaultLocale;
+
+  ref.state = lookupAppLocalizations(locale);
+
+  return ref.state;
+});
+
+class L10n {
+  static final all = [
+    const Locale('en', 'US'),
+    const Locale('fr', "FR"),
+  ];
+
+  static Locale get defaultLocale => const Locale('en', 'US');
+}
