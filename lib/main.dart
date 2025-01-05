f54689c6db9f113b3c16e79b02735f8367ad903c
ibra <albakov.ibraguim@gmail.com>
2025-01-05 01:33:56 +0100

    🗃️added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'dart:async';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'config/app_runner.dart';
 
-void main() {
+void main() async {
   runZonedGuarded<Future<void>>(() async {
     WidgetsFlutterBinding.ensureInitialized();
+
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: "AIzaSyCvy536IiKX_fvDwdOHBJfNv5f_1GCIpoA",
+          authDomain: "barber-shop-web-app-5c196.firebaseapp.com",
+          projectId: "barber-shop-web-app-5c196",
+          storageBucket: "barber-shop-web-app-5c196.firebasestorage.app",
+          messagingSenderId: "765324286502",
+          appId: "1:765324286502:web:b0bfc070b1a843b9ec24e7"),
+    );
 
     AppRunner.runWebApp();
   }, (error, stack) {
